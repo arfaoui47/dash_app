f6866f33421ece181232c2c19557070875b26b71
Atef Arfaoui <arfaoui47@gmail.com>
2018-10-30 23:10:42 +0100

    update provision

--- provision.py
+++ provision.py
@@ -1,4 +1,4 @@
-from fabric.api import task, run, cd, hosts, sudo
+from fabric.api import task, run, cd, hosts, sudo, settings
 
 
 #TODO add host server IP address
@@ -10,9 +10,10 @@ GIT_REPO = ""
 @task
 @hosts(HOST)
 def update_create_user():
-    sudo("sudo apt-get update")
-    sudo("sudo apt-get install python3-pip python3-dev nginx")
-    sudo("sudo -H pip3 install virtualenv")
+    with settings(warn_only=True):
+        sudo("apt-get update")
+        sudo("apt-get install python3-pip python3-dev nginx")
+        sudo("pip3 install virtualenv")
 
     sudo("adduser dash")
     sudo("usermod -aG sudo dash")
